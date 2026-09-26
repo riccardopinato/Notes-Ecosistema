f733c8fe3402bdb109acc07a173ebf0adeadda53
riccardopinato <riccardo121296@gmail.com>
2026-09-26 11:57:38 +0200

    feat(intelligence): provide derivative sidecar store

--- flutter_app/lib/src/state/workspace_controller.dart
+++ flutter_app/lib/src/state/workspace_controller.dart
@@ -4,6 +4,7 @@ import 'package:uuid/uuid.dart';
 import '../data/legacy_notes_database.dart';
 import '../data/property_store.dart';
 import '../data/knowledge_store.dart';
+import '../data/derivative_store.dart';
 import '../domain/backup.dart';
 import '../domain/library.dart';
 import '../domain/note.dart';
@@ -23,6 +24,12 @@ final propertyStoreProvider = Provider<PropertyStore>((ref) {
 
 final knowledgeStoreProvider = Provider<KnowledgeStore>((ref) {
   final store = KnowledgeStore();
+  ref.onDispose(store.close);
+  return store;
+});
+
+final derivativeStoreProvider = Provider<DerivativeStore>((ref) {
+  final store = DerivativeStore();
   ref.onDispose(store.close);
   return store;
 });
